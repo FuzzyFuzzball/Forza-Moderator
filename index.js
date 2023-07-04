f8387e43998adec2c7b9b0b0560f5a18a29aa179
Dexxieee <130196960+FuzzyFuzzball@users.noreply.github.com>
2023-07-04 19:53:43 +1200

    start of project with index & .gitignore

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Client, IntentsBitField, ActivityType } = require('discord.js')
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildPresences
+    ],
+    ws: {
+        properties: {
+            browser: 'Discord iOS'
+        }
+    },
+})
+require('dotenv').config()
+
+client.on('ready', () => {
+    console.log(`${client.user.username} is online.`)
+
+    client.user.setActivity({
+        name: "myself being coded",
+        type: ActivityType.Watching
+    })
+
+    client.user.setStatus('dnd')
+})
+
+client.login(process.env.TOKEN)
